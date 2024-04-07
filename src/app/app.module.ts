f6bad8a8aa7acbc2d579e127bd28b45d26f575ae
Adams Castillo <adams.santos.ap@gmail.com>
2024-04-06 22:48:37 -0300

    primeira parte do hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HeroComponent
+
 
 
   ],
